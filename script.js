8586b5bc7ebb62ad211752b3ad2e3c8c73b54b78
James2166 <jlh3ndr@gmail.com>
2021-11-23 09:30:56 -0700

    Final Update

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 //Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = '1-XnhbIG57EQ-ft8dN1uIvzT6ABxEJdG7pFg3XIwnWk';
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+        console.log('ready =', ready);
+    }
+}
 
 // Helper Function to Set Attributes on DOM Elements
 function setAttributes(element, attributes) {
@@ -17,6 +30,8 @@ function setAttributes(element, attributes) {
 
 // Create Elements For Links & Photos, Add to DOM
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
     // Run function for each object in PhotosArray
     photosArray.forEach((photo) => {
         // Create <a> to link to Unsplash
@@ -32,6 +47,8 @@ function displayPhotos() {
             alt: photo.alt_description,
             title: photo.alt_description
         });
+        // Event Listener, Check when each is finished loading
+        img.addEventListener('load', imageLoaded);
         // Put <img> inside <a>, then put both inside imageContainer element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -49,6 +66,15 @@ async function getPhotos() {
         // Catch Error Here
     }
 }
+
+//  Check to see if scrolling near bottom of page, Load more Photots
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight -1000 && ready) {
+        ready = false;
+        getPhotos();
+
+    }
+})
 
 // On Load
 getPhotos();
